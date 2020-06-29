817071f89ad04c655068ee43bf54e7c9fe458887
Brittni Watkins <blwatkins@smu.edu>
2020-06-29 15:26:21 -0500

    Make Shape class abstract Add color component Add constructor and methods

--- Euler-Particles/src/shape/Shape.java
+++ Euler-Particles/src/shape/Shape.java
@@ -3,9 +3,27 @@ package shape;
 import processing.core.PApplet;
 import processing.core.PVector;
 
-public class Shape {
+import color.Color;
+
+public abstract class Shape {
     private PApplet p;
     private PVector position;
     private PVector speed;
+    private Color color;
 
+    public Shape(PApplet p) {
+        position = new PVector(p.random(p.width), p.random(p.height));
+        speed = new PVector(p.random(-5, 5), p.random(-5, 5));
+        color = new Color(p, p.color(0, 0, 255));
+    }
+
+    public void setColor(int color) {
+        this.color.setColor(color);
+    }
+
+    public abstract void display();
+
+    public void move() {
+        position.add(speed);
+    }
 }
