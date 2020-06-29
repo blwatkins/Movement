f60460f7efc58fe7faf1c139137565d81f14925a
Brittni Watkins <blwatkins@smu.edu>
2020-06-29 15:17:15 -0500

    Add instance variables to Shape class

--- Euler-Particles/src/shape/Shape.java
+++ Euler-Particles/src/shape/Shape.java
@@ -1,4 +1,11 @@
 package shape;
 
+import processing.core.PApplet;
+import processing.core.PVector;
+
 public class Shape {
+    private PApplet p;
+    private PVector position;
+    private PVector speed;
+
 }
