9656148458ff6e06c354e5f6a107b1ebcb1e57a0
Brittni Watkins <blwatkins@smu.edu>
2020-06-29 15:15:39 -0500

    Add processing base code to EulerParticles class.

--- Euler-Particles/src/EulerParticles.java
+++ Euler-Particles/src/EulerParticles.java
@@ -1,2 +1,21 @@
-public class EulerParticles {
+import processing.core.PApplet;
+
+public class EulerParticles extends PApplet {
+
+    public static void main(String[] args) {
+        String[] processingArgs = {"EulerParticles"};
+        PApplet.main(processingArgs);
+    }
+
+    public void settings() {
+        size(800, 800);
+    }
+
+    public void setup() {
+
+    }
+
+    public void draw() {
+
+    }
 }
