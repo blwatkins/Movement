432864b1b784c6f1b1d6785ee1f5f21e2e5f3c61
Brittni Watkins <blwatkins@smu.edu>
2020-06-30 09:18:09 -0500

    Have Circle class inherit from Shape class

--- Euler-Particles/src/shape/Circle.java
+++ Euler-Particles/src/shape/Circle.java
@@ -1,4 +1,14 @@
 package shape;
 
-public class Circle {
+import processing.core.PApplet;
+
+public class Circle extends Shape {
+
+    public Circle(PApplet p) {
+        super(p);
+    }
+
+    public void display() {
+
+    }
 }
