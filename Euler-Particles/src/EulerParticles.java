67e5a722ec20aaa61efb6c912b7eefdf5f2e1fd1
Brittni Watkins <blwatkins@smu.edu>
2020-06-30 09:31:22 -0500

    Add shapes to main class

--- Euler-Particles/src/EulerParticles.java
+++ Euler-Particles/src/EulerParticles.java
@@ -1,6 +1,10 @@
 import processing.core.PApplet;
 
+import shape.Circle;
+import shape.Shape;
+
 public class EulerParticles extends PApplet {
+    Shape[] shapes;
 
     public static void main(String[] args) {
         String[] processingArgs = {"EulerParticles"};
@@ -12,10 +16,18 @@ public class EulerParticles extends PApplet {
     }
 
     public void setup() {
+        shapes = new Shape[100];
 
+        for (int i = 0; i < shapes.length; i++) {
+            shapes[i] = new Circle(this);
+        }
     }
 
     public void draw() {
-
+        background(0);
+        for (Shape s: shapes) {
+            s.display();
+            s.move();
+        }
     }
 }
