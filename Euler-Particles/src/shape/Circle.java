7013a0fe62f5ef5933fac4e3bc2489f32bd54584
Brittni Watkins <blwatkins@smu.edu>
2020-06-30 10:38:43 -0500

    Make Circle inherit from RadialShape

--- Euler-Particles/src/shape/Circle.java
+++ Euler-Particles/src/shape/Circle.java
@@ -3,13 +3,10 @@ package shape;
 import processing.core.PApplet;
 import processing.core.PVector;
 
-public class Circle extends Shape {
-    private float radius;
+public class Circle extends RadialShape {
 
     public Circle(PApplet p) {
         super(p);
-        radius = p.random(5, 50);
-        setPosition(new PVector(p.random(radius, p.width - radius), p.random(radius, p.height - radius)));
     }
 
     public void display() {
@@ -19,21 +16,7 @@ public class Circle extends Shape {
         p.fill(getColor());
         p.pushMatrix();
         p.translate(position.x, position.y);
-        p.ellipse(0, 0, radius * 2, radius * 2);
+        p.ellipse(0, 0, getRadius() * 2, getRadius() * 2);
         p.popMatrix();
-    }
-
-    protected void bounce() {
-        PApplet p = getPApplet();
-        PVector position = getPosition();
-        PVector speed = getSpeed();
-
-        if (position.x + radius >= p.width || position.x - radius <= 0) {
-            speed.x *= -1;
-        }
-
-        if (position.y + radius >= p.height || position.y - radius <= 0) {
-            speed.y *= -1;
-        }
     }
 }
