69294a7fdf6dad941b35f98c61389d25dc267b67
Brittni Watkins <blwatkins@smu.edu>
2020-06-30 10:34:17 -0500

    Have Polygon inherit from Shape class

--- Euler-Particles/src/shape/Polygon.java
+++ Euler-Particles/src/shape/Polygon.java
@@ -1,4 +1,31 @@
 package shape;
 
-public class Polygon {
+import processing.core.PApplet;
+import processing.core.PVector;
+
+public class Polygon extends Shape {
+    private float radius;
+
+    public Polygon(PApplet p) {
+        super(p);
+        setPosition(new PVector(p.random(radius, p.width - radius), p.random(radius, p.height - radius)));
+    }
+
+    public void display() {
+
+    }
+
+    protected void bounce() {
+        PApplet p = getPApplet();
+        PVector position = getPosition();
+        PVector speed = getSpeed();
+
+        if (position.x + radius >= p.width || position.x - radius <= 0) {
+            speed.x *= -1;
+        }
+
+        if (position.y + radius >= p.height || position.y - radius <= 0) {
+            speed.y *= -1;
+        }
+    }
 }
