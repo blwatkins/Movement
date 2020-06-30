174510f895ab8b16ac928da8d4af4bd2f6b772f9
Brittni Watkins <blwatkins@smu.edu>
2020-06-30 12:06:29 -0500

    Add display background and displayShapes methods. Add change background functionality

--- Euler-Particles/src/EulerParticles.java
+++ Euler-Particles/src/EulerParticles.java
@@ -6,6 +6,8 @@ import shape.ShapeFactory;
 public class EulerParticles extends PApplet {
     Shape[] shapes;
     ShapeFactory shapeFactory;
+    boolean hasBlackBackground;
+    boolean showTrails;
 
     public static void main(String[] args) {
         String[] processingArgs = {"EulerParticles"};
@@ -19,6 +21,8 @@ public class EulerParticles extends PApplet {
     public void setup() {
         shapeFactory = new ShapeFactory(this);
         shapes = new Shape[100];
+        hasBlackBackground = true;
+        showTrails = false;
 
         for (int i = 0; i < shapes.length; i++) {
             shapes[i] = shapeFactory.getShape();
@@ -26,7 +30,37 @@ public class EulerParticles extends PApplet {
     }
 
     public void draw() {
-        background(0);
+        displayBackground();
+        displayShapes();
+    }
+
+    public void keyPressed() {
+
+        if (key == 'a') {
+            hasBlackBackground = !hasBlackBackground;
+        } else if (key == 's') {
+            showTrails = !showTrails;
+        }
+    }
+
+    private void displayBackground() {
+        int color;
+
+        if (hasBlackBackground) {
+            color = color(0);
+        } else {
+           color = color(255);
+        }
+
+        if (showTrails) {
+            fill(color, 25);
+            rect(-10, -10, width + 10, height + 10);
+        } else {
+            background(color);
+        }
+    }
+
+    private void displayShapes() {
 
         for (Shape s: shapes) {
             s.display();
