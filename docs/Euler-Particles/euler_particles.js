2974818b97de93221b0d2e063626492efd529504
Brittni Watkins <blwatkins@smu.edu>
2020-08-05 15:56:58 -0500

    Add instructions. Add change background color functionality.

--- docs/Euler-Particles/euler_particles.js
+++ docs/Euler-Particles/euler_particles.js
@@ -1,7 +1,9 @@
 // Euler Particles Main Sketch
+// Press 'a' to change background to black or white
 
 var shapes;
 var shapeCount;
+var hasBlackBackground;
 var colorGenerator;
 var colorGeneratorFactory;
 
@@ -10,14 +12,23 @@ function setup() {
     frameRate(60);
     shapes = [];
     shapeCount = 100;
+    hasBlackBackground = true;
     colorGeneratorFactory = new ColorGeneratorFactory();
     colorGenerator = colorGeneratorFactory.getRandomColorGenerator();
     createShapes();
+    createInstructions();
 }
 
 function draw() {
-    displayBlackground();
+    displayBackground();
     displayShapes();
+}
+
+function keyTyped() {
+
+    if (key == 'a') {
+        hasBlackBackground = !hasBlackBackground;
+    }
 }
 
 function createShapes() {
@@ -29,8 +40,29 @@ function createShapes() {
     }
 }
 
-function displayBlackground() {
-    background(0);
+function createInstructions() {
+    let div = createElement('div');
+    let h1 = createElement('h1', 'Instructions');
+    let instructionList = createElement('ul');
+    let instructions = [];
+    instructions.push(createElement('li', "Press the 'a' key to change the background to black or white"));
+    div.id('instructions');
+    h1.parent(div);
+    instructionList.parent(div);
+
+    instructions.forEach((instruction) => {
+        instruction.parent(instructionList);
+    });
+}
+
+function displayBackground() {
+    let c = color(255);
+
+    if (hasBlackBackground) {
+        c = color(0);
+    }
+
+    background(c);
 }
 
 function displayShapes() {
