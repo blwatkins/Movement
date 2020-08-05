f809b395726db1b17f5023b914a93c7654d72a66
Brittni Watkins <blwatkins@smu.edu>
2020-08-05 15:26:23 -0500

    Add constructor and methods to Shape class.

--- docs/Euler-Particles/shape/shape.js
+++ docs/Euler-Particles/shape/shape.js
@@ -1,3 +1,29 @@
 class Shape {
 
+    constructor() {
+        this.position = createVector(random(width), random(height));
+        this.speed = createVector(random(-5, 5), random(-5, 5));
+        this.color = new Color(color(0, 0, 255));
+    }
+
+    getColor() {
+        return this.color.getColor();
+    }
+
+    setColor(color) {
+        this.color.setColor(color);
+    }
+
+    display() {
+
+    }
+
+    move() {
+        this.position.add(speed);
+        this.bounce();
+    }
+
+    bounce() {
+
+    }
 }
