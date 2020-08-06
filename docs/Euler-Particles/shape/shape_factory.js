cdfd06867acb3b25fc45c0ea431ef25503de7eb4
Brittni Watkins <blwatkins@smu.edu>
2020-08-06 12:21:15 -0500

    Add constructor and methods to ShapeFactory class.

--- docs/Euler-Particles/shape/shape_factory.js
+++ docs/Euler-Particles/shape/shape_factory.js
@@ -1,3 +1,49 @@
 class ShapeFactory {
 
+    constructor() {
+        this._CIRCLE = 0;
+        this._POLYGON = 1;
+        this._STAR = 2;
+        this.type = this._CIRCLE;
+        this.sides = 0;
+        this.randomize();
+    }
+
+    getShape() {
+        let shape = null;
+
+        switch (this.type) {
+            case this._POLYGON:
+                shape = new Polygon(this.sides);
+                break;
+            case this._STAR:
+                shape = new Star(this.sides);
+                break;
+            default:
+                shape = new Circle();
+                break;
+        }
+
+        return shape;
+    }
+
+    randomize() {
+        let r = int(random(300));
+
+        switch (r % 3) {
+            case 1:
+                this.type = this._CIRCLE;
+                break;
+            case 2:
+                this.type = this._POLYGON;
+                break;
+            default:
+                this.type = this._STAR;
+                break;
+        }
+
+        this.sides = int(random(3, 12));
+    }
+
+
 }
