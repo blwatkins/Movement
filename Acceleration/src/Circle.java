61a511b4865445fcb47680fdc7a574a0ee2fae01
Brittni Watkins <blwatkins@smu.edu>
2020-08-06 13:16:31 -0500

    Add color object to Circle class

--- Acceleration/src/Circle.java
+++ Acceleration/src/Circle.java
@@ -1,11 +1,14 @@
 import processing.core.PApplet;
 import processing.core.PVector;
 
+import color.Color;
+
 public class Circle {
     private PApplet p;
     private PVector position;
     private PVector speed;
     private PVector acceleration;
+    private Color color;
     private float radius;
     private float maxSpeed;
     private float maxAcceleration;
@@ -17,6 +20,7 @@ public class Circle {
         position = new PVector(p.random(radius, p.width - radius), p.random(radius, p.height - radius));
         speed = new PVector(p.random(-5, 5), p.random(-5, 5));
         acceleration = new PVector(0, 0);
+        color = new Color(p, p.color(0, 0, 255));
         maxSpeed = 8;
         maxAcceleration = 0.5f;
         deltaAcceleration = 0.2f;
