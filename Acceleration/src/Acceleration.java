64b9d17f8d313c3da713e82c9d0052489ecd4d37
Brittni Watkins <blwatkins@smu.edu>
2020-08-06 12:48:16 -0500

    Add initial class for Acceleration project.

---
+++ Acceleration/src/Acceleration.java
@@ -0,0 +1,21 @@
+import processing.core.PApplet;
+
+public class Acceleration extends PApplet {
+
+    public static void main(String[] args) {
+        String[] processingArgs = {"Acceleration"};
+        PApplet.main(processingArgs);
+    }
+
+    public void settings() {
+        size(800, 800);
+    }
+
+    public void setup() {
+
+    }
+
+    public void draw() {
+
+    }
+}
