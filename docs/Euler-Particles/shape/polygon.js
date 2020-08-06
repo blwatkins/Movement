7536eeda2dc145c11174e73a0a4f6caf96d8946e
Brittni Watkins <blwatkins@smu.edu>
2020-08-06 12:01:31 -0500

    Add methods to Polygon class.

--- docs/Euler-Particles/shape/polygon.js
+++ docs/Euler-Particles/shape/polygon.js
@@ -2,5 +2,35 @@ class Polygon extends RadialShape {
 
     constructor(sides) {
         super();
+        this.vertices = [];
+        this._initVertices(sides);
+    }
+
+    _initVertices(sides) {
+        let theta = 0;
+        sides = constrain(3, 20);
+        let deltaTheta = TWO_PI / sides;
+
+        for (let i = 0; i < sides; i++) {
+            let x = cos(theta) * this.radius;
+            let y = sin(theta) * this.radius;
+            theta += deltaTheta;
+            this.vertices.push(createVector(x, y));
+        }
+    }
+
+    display() {
+        push();
+        noStroke();
+        fill(this.color.getColor());
+        translate(this.position.x, this.position.y);
+        beginShape();
+
+        this.vertices.forEach((v) => {
+            vertex(v);
+        });
+
+        endShape(CLOSE);
+        pop();
     }
 }
