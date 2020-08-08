bd976a7c4a0fe16ccafca547e1568efa327c78db
Brittni Watkins <blwatkins@smu.edu>
2020-08-08 15:49:12 -0500

    Add initial files for Acceleration project.

---
+++ docs/Acceleration/acceleration.js
@@ -0,0 +1,10 @@
+// Acceleration Sketch
+
+function setup() {
+    createCanvas(windowWidth-20, windowHeight-20);
+    frameRate(60);
+}
+
+function draw() {
+    ellipse(mouseX, mouseY, 50, 50);
+}
