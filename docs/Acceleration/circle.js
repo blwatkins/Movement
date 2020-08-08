4ded9109323b7a0271636e1259e6e1bc2534340b
Brittni Watkins <blwatkins@smu.edu>
2020-08-08 15:59:15 -0500

    Add constructor to Circle class.

--- docs/Acceleration/circle.js
+++ docs/Acceleration/circle.js
@@ -1,3 +1,13 @@
 class Circle {
 
+    constructor() {
+        this.radius = 5;
+        this.postion = createVector(random(this.radius, width - radius), random(this.radius, height - radius));
+        this.speed = createVector(random(-5, 5), random(-5, 5));
+        this.acceleration = createVector(0, 0);
+        this.color = new Color(color(0, 0, 255));
+        this.maxSpeed = 8;
+        this.maxAcceleration = 0.5;
+        this.deltaAcceleration = 0.2;
+    }
 }
