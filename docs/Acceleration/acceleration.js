c9ae2ef67468a79390354d9cb359700fe9631237
Brittni Watkins <blwatkins@smu.edu>
2020-08-08 16:21:44 -0500

    Add background effects.

--- docs/Acceleration/acceleration.js
+++ docs/Acceleration/acceleration.js
@@ -1,17 +1,32 @@
 // Acceleration Sketch
+// Press 'a' to change the background to black or white
+// Press 's' to turn the trails on and off
 
 var circles;
+var hasBlackBackground;
+var isShowingTrails;
 
 function setup() {
     createCanvas(windowWidth-20, windowHeight-20);
     frameRate(60);
     circles = [];
+    hasBlackBackground = true;
+    isShowingTrails = false;
     createCircles();
 }
 
 function draw() {
     displayBackground();
     displayCircles();
+}
+
+function keyTyped() {
+
+    if (key === 'a') {
+        hasBlackBackground = !hasBlackBackground;
+    } else if (key === 's') {
+        isShowingTrails = !isShowingTrails;
+    }
 }
 
 function createCircles() {
@@ -23,7 +38,18 @@ function createCircles() {
 }
 
 function displayBackground() {
-    background(0);
+    let color = 255;
+
+    if (hasBlackBackground) {
+        color = 0;
+    }
+
+    if (isShowingTrails) {
+        fill(color, 25);
+        rect(-10, -10, width + 10, height + 10);
+    } else {
+        background(color)
+    }
 }
 
 function displayCircles() {
