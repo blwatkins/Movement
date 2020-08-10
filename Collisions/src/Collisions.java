37fbeb0446e0f51e79358f29a3089104b1c3e9e3
Brittni Watkins <blwatkins@smu.edu>
2020-08-10 15:50:44 -0500

    Add code to create Processing sketch to Collisions class

--- Collisions/src/Collisions.java
+++ Collisions/src/Collisions.java
@@ -1,2 +1,21 @@
-public class Collisions {
+import processing.core.PApplet;
+
+public class Collisions extends PApplet {
+
+    public static void main(String[] args) {
+        String[] processingArgs = {"Collisions"};
+        PApplet.main(processingArgs);
+    }
+
+    public void settings() {
+        size(800, 800);
+    }
+
+    public void setup() {
+
+    }
+
+    public void draw() {
+
+    }
 }
