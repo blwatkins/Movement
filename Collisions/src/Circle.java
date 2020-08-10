4b3cb22e5f4e2a6a4017e3de6e1eb14bbb18c8c0
Brittni Watkins <blwatkins@smu.edu>
2020-08-10 16:33:14 -0500

    Add circles and ColorGenerator to main sketch.

--- Collisions/src/Circle.java
+++ Collisions/src/Circle.java
@@ -13,11 +13,15 @@ public class Circle {
 
     public Circle(PApplet p) {
         this.p = p;
-        radius = p.random(5, 100);
+        radius = p.random(5, 50);
         mass = radius;
         position = new PVector(p.random(radius, p.width - radius), p.random(radius, p.height - radius));
         velocity = new PVector(p.random(-5, 5), p.random(-5, 5));
         color = new Color(p, p.color(0, 0, 255));
+    }
+
+    public void setColor(int color) {
+        this.color.setColor(color);
     }
 
     public void display() {
--- Collisions/src/Collisions.java
+++ Collisions/src/Collisions.java
@@ -1,6 +1,12 @@
 import processing.core.PApplet;
 
+import color.ColorGenerator;
+import color.ColorGeneratorFactory;
+
 public class Collisions extends PApplet {
+    private Circle[] circles;
+    private ColorGeneratorFactory colorGeneratorFactory;
+    private ColorGenerator colorGenerator;
 
     public static void main(String[] args) {
         String[] processingArgs = {"Collisions"};
@@ -12,10 +18,36 @@ public class Collisions extends PApplet {
     }
 
     public void setup() {
-
+        circles = new Circle[20];
+        colorGeneratorFactory = new ColorGeneratorFactory(this);
+        colorGenerator = colorGeneratorFactory.getRandomColorGenerator();
+        createCircles();
     }
 
     public void draw() {
+        displayBackground();
+        displayCircles();
+    }
 
+    private void createCircles() {
+
+        for (int i = 0; i < circles.length; i++) {
+            Circle circle = new Circle(this);
+            int color = colorGenerator.getRandomColor();
+            circle.setColor(color);
+            circles[i] = circle;
+        }
+    }
+
+    private void displayBackground() {
+        background(0);
+    }
+
+    private void displayCircles() {
+
+        for (Circle circle: circles) {
+            circle.display();
+            circle.move();
+        }
     }
 }
