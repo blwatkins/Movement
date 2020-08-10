96efbff8c1797f9227f6552da5aa7f5ccf8d9733
Brittni Watkins <blwatkins@smu.edu>
2020-08-10 16:27:09 -0500

    Add display and move methods to Circle class.

--- Collisions/src/Circle.java
+++ Collisions/src/Circle.java
@@ -19,4 +19,32 @@ public class Circle {
         velocity = new PVector(p.random(-5, 5), p.random(-5, 5));
         color = new Color(p, p.color(0, 0, 255));
     }
+
+    public void display() {
+        p.noStroke();
+        p.fill(color.getColor());
+        p.ellipse(position.x, position.y, radius * 2, radius * 2);
+    }
+
+    public void move() {
+        incrementVectors();
+        bounce();
+    }
+
+    private void incrementVectors() {
+        position.add(velocity);
+    }
+
+    private void bounce() {
+
+        if (position.x + radius >= p.width || position.x - radius <= 0) {
+            velocity.x *= -1;
+            position.x = PApplet.constrain(position.x, 0, p.width);
+        }
+
+        if (position.y + radius >= p.height || position.y - radius <= 0) {
+            velocity.y *= -1;
+            position.y = PApplet.constrain(position.y, 0, p.height);
+        }
+    }
 }
