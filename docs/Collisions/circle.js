71446fc126d52ab883b7b57936bfe6498da39ecc
Brittni Watkins <blwatkins@smu.edu>
2020-08-18 15:28:26 -0500

    Add methods for bouncing circles off of each other.

--- docs/Collisions/circle.js
+++ docs/Collisions/circle.js
@@ -8,6 +8,22 @@ class Circle {
         this.color = new Color(color(0, 0, 255));
     }
 
+    getRadius() {
+        return this.radius;
+    }
+
+    getMass() {
+        return this.mass;
+    }
+
+    getPosition() {
+        return this.position;
+    }
+
+    getVelocity() {
+        return this.velocity;
+    }
+
     setColor(color) {
         this.color.setColor(color);
     }
@@ -21,6 +37,17 @@ class Circle {
     move() {
         this._incrementVectors();
         this._bounce();
+    }
+
+    collideAndBounce(circle) {
+        let totalRadius = this.radius + circle.getRadius();
+        let difference = p5.Vector.sub(this.position, circle.getPosition());
+        let distance = difference.mag();
+        let hasCollision = distance <= totalRadius;
+
+        if (hasCollision) {
+            this._bounceCircle(circle);
+        }
     }
 
     _incrementVectors() {
@@ -42,5 +69,23 @@ class Circle {
             this.velocity.set(this.velocity.x, velocityY);
             this.position.set(this.position.x, y);
         }
+    }
+
+    _bounceCircle(circle) {
+        let totalRadius = this.radius + circle.getRadius();
+        let difference = p5.Vector.sub(this.position, circle.getPosition());
+        let distance = difference.mag();
+        let distanceCorrection = (totalRadius - distance) / 2;
+        let correction = difference.normalize().mult(distanceCorrection);
+        this.position.add(correction);
+        circle.getPosition().sub(correction);
+        let tempVelocityX = this.velocity.x;
+        let tempVelocityY = this.velocity.y;
+        let velocityX = (((this.mass - circle.getMass()) * this.velocity.x) + (2 * circle.getMass() * circle.getVelocity().x)) / (this.mass + circle.getMass());
+        let velocityY = (((this.mass - circle.getMass()) * this.velocity.y) + (2 * circle.getMass() * circle.getVelocity().y)) / (this.mass + circle.getMass());
+        let circleVelocityX = (((circle.getMass() - this.mass) * circle.getVelocity().x) + (2 * this.mass * tempVelocityX)) / (this.mass + circle.getMass());
+        let circleVelocityY = (((circle.getMass() - this.mass) * circle.getVelocity().y) + (2 * this.mass * tempVelocityY)) / (this.mass + circle.getMass());
+        this.velocity.set(velocityX, velocityY);
+        this.circle.getVelocity().set(circleVelocityX, circleVelocityY);
     }
 }
