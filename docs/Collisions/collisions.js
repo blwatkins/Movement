b9c000698b0b9336f7558faa8467c6aedc52f43d
Brittni Watkins <blwatkins@smu.edu>
2020-08-19 08:47:38 -0500

    Add background color change functionality.

--- docs/Collisions/collisions.js
+++ docs/Collisions/collisions.js
@@ -1,18 +1,29 @@
 // Collisions Sketch
+// Press 'a' to change the background to black or white
 
 var circles;
+var hasBlackBackground;
 
 function setup() {
     createCanvas(windowWidth-20, windowHeight-20);
     frameRate(60);
     circles = [];
+    hasBlackBackground = true;
     createCircles();
+    createInstructions();
 }
 
 function draw() {
     displayBackground();
     displayCircles();
     checkForCircleCollisions();
+}
+
+function keyTyped() {
+
+    if (key === 'a') {
+        toggleBackgroundColor();
+    }
 }
 
 function createCircles() {
@@ -23,8 +34,31 @@ function createCircles() {
     }
 }
 
+function createInstructions() {
+    let div = createElement('div');
+    let h1 = createElement('h1', 'Instructions');
+    let instructionList = createElement('ul');
+    let instructions = [];
+    instructions.push(createElement('li', "Press the 'a' key to change the background to black or white"));
+    instructions.push(createElement('li', "Press the 's' key to activate and deactivate trails"));
+    instructions.push(createElement('li', "Press the 'd' key to change the color scheme"));
+    div.id('instructions');
+    h1.parent(div);
+    instructionList.parent(div);
+
+    instructions.forEach((instruction) => {
+        instruction.parent(instructionList);
+    });
+}
+
 function displayBackground() {
-    background(0);
+    let color = 255;
+
+    if (hasBlackBackground) {
+        color = 0;
+    }
+
+    background(color);
 }
 
 function displayCircles() {
@@ -43,4 +77,8 @@ function checkForCircleCollisions() {
             circles[i].collideAndBounce(circles[k]);
         }
     }
+}
+
+function toggleBackgroundColor() {
+    hasBlackBackground = !hasBlackBackground;
 }
